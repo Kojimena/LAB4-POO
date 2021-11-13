19a42cc2bb8e6080206a6c98ff9991e863b2b167
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-11-13 17:01:42 -0600

    vista

---
+++ Vista.java
@@ -0,0 +1,23 @@
+/**
+* @author:  Mark Albrand & Jimena Hernández 
+* @version: 13-Nov-21
+* @file: Vista.java
+* Esta clase es la vista del programa.Imprime resultados y pide datos.
+**/
+
+//imports
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException; 
+
+public class Vista {
+
+    /**
+    * @param: String
+    * imprime texto
+    **/ 
+    public void mensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+
+}
