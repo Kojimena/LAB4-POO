44c965a35b8a601d7dc38c180fe9effff5445167
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-11-13 18:48:36 -0600

    canciones

--- radios/Radio.java
+++ radios/Radio.java
@@ -5,6 +5,7 @@ public abstract class Radio {
     protected double[] emisorasGuardadas = new double[50];
     protected double emisoraactual= 87.5;
     protected String[][] contactos = {{"Olivia Rodrigo", "41395467"},{"Stephen Smitz","42118956"},{"Pepe Juarez","42245677"},{"Mark Albrand","34567764"},{"Jimena Hernández","41303714"}};
+    protected String[][][] canciones = {{{"Been Away", "3.44", "Brent Faiyaz","Pop"},{"Dead man walkin", "4.07", "Brent Faiyaz","Pop"} ,{"Just a Stranger ", "2.58", "Kali Uchis","Pop"},{"Dark red ", "2.53", "Steve lacy","Hip-hop"},{"Carry me home ", "5.22", "Jorja Smith","Pop"}},{{"Lo siento BB:/", "3.27", "Bad bunny", "Reggaeton"}, {"Dakiti", "3.25", "Bad bunny","Reggaeton"}, {"La canción", "4.02", "J balvin", "Reggaeton"}, {"Red", "3.43", "Taylor Swift", "Pop"}, {"La fama", "3.08", "ROSALIA", "Reggaeton"}}};
     protected Boolean encendido = false;
     protected Boolean enLlamada = false;
     protected String tipoFrecuencia = "FM";
