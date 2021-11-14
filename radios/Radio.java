2574f7bfc9a208dc58ceb4b399b2bd458aa732cc
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-11-13 18:12:39 -0600

    contactos

--- radios/Radio.java
+++ radios/Radio.java
@@ -3,8 +3,12 @@ package radios;
 public abstract class Radio {
     protected int contactoactual= -1;
     protected String[] emisorasGuardadas = new String[50];
-    protected String[][] contactos = new String[5][2];
+    protected String[][] contactos = {{"Olivia Rodrigo", "41395467"},{"Stephen Smitz","42118956"},{"Pepe Juarez","42245677"},{"Mark Albrand","34567764"},{"Jimena Hernández","41303714"}};
     protected Boolean encendido = false;
     protected Boolean enLlamada = false;
     protected String tipoFrecuencia = "FM";
+
+
+    public Radio(){
+    }
 }
