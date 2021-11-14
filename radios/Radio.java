3652c0ec5ac0f20f22e9c8262ed84caf599976d2
Mark Albrand <mark.albrand5@gmail.com>
2021-11-13 18:07:27 -0600

    Update Radio.java

--- radios/Radio.java
+++ radios/Radio.java
@@ -3,7 +3,7 @@ package radios;
 public abstract class Radio {
     protected int contactoactual= -1;
     protected String[] emisorasGuardadas = new String[50];
-    protected String[][] contactos;
+    protected String[][] contactos = new String[5][2];
     protected Boolean encendido = false;
     protected Boolean enLlamada = false;
     protected String tipoFrecuencia = "FM";
