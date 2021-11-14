b134778b80fea611faf90d58b72b14183da0b653
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-11-13 18:26:02 -0600

    radio

--- radios/Radio.java
+++ radios/Radio.java
@@ -3,12 +3,44 @@ package radios;
 public abstract class Radio {
     protected int contactoactual= -1;
     protected String[] emisorasGuardadas = new String[50];
+    protected double emisoraactual= 87.5;
     protected String[][] contactos = {{"Olivia Rodrigo", "41395467"},{"Stephen Smitz","42118956"},{"Pepe Juarez","42245677"},{"Mark Albrand","34567764"},{"Jimena Hernández","41303714"}};
     protected Boolean encendido = false;
     protected Boolean enLlamada = false;
     protected String tipoFrecuencia = "FM";
 
 
-    public Radio(){
+   abstract String pantalla();
+
+   public String cambiarAMFM(int frecuencia){
+    String mensaje = " ";
+    switch(frecuencia){
+        case 1:
+        mensaje= " Se ha cambiado a la frecuencia FM";
+        this.tipoFrecuencia= "FM";
+        break;
+
+        case 2:
+        mensaje= " Se ha cambiado a la frecuencia AM";
+        this.tipoFrecuencia= "AM";
+        break; 
+
     }
+    return mensaje;
+   }
+
+   public String cambiarEmisora(){
+     String mensaje="";
+    if(emisoraactual!= 108.0){
+        emisoraactual += 0.5;
+        mensaje = "Se ha cambiado a la emisora: "+ emisoraactual;
+    }else{
+        emisoraactual= 87.5;
+        mensaje = "Se ha cambiado a la emisora: "+ emisoraactual;
+    }
+    return mensaje;
+   }
+
+
+   
 }
