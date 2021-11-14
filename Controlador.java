42fd35e809dad0a77d155f3a4307059ffbe0290f
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-11-14 16:03:39 -0600

    controlador

--- Controlador.java
+++ Controlador.java
@@ -10,6 +10,6 @@ public class Controlador {
     
     private static Vista vista = new Vista();
     public static void main(String[] args) {
-        
+        int tipoderadio = vista.tipoRadio();
     }
 }
